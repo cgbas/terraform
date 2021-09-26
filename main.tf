0c8a30e3c9c5b5fa9f770359a0a8ce8618136c1c
Christian Bernini <christian.alves@zup.com.br>
2021-09-26 17:46:20 -0300

    initial terraform setup

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    count = 3
+    ami = "ami-087c17d1fe0178315"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags {
+        name = "dev${count.index}"
+    }
+  
+}
