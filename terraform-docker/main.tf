46f9be0a76d984273cb753da675b87a1c0c94edb
Christian <cgbas1987@gmail.com>
2022-03-08 22:47:20 +0000

    outputs

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,10 +1,34 @@
 terraform {
-    required_providers {
-        docker = {
-            source = "kreuzewerker/docker"
-            version = "~> 2.16.0"
-        }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.16.0"
     }
+  }
 }
 
 provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+
+}
+resource "docker_container" "nodered_container" {
+  name  = "nodered"
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    external = 1880
+  }
+
+}
+
+output "IP-Address" {
+  value =  docker_container.nodered_container.ip_address
+  description = "Container internal IP address"
+  
+}
+output "Container-Name" {
+  value = docker_container.nodered_container.name
+  description = "Container name"
+}
